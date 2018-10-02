e57e7b2cfbc1f3db741a3127e329e12c3a60bb78
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-02 15:47:03 +0200

    Move API address and query params to a separate file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,12 @@
 import React, {Component} from 'react';
 import moment from 'moment';
+import * as api from 'constants/apiQueries';
 
 import loading from 'images/loading.gif';
 import Header from './Header';
 import EpisodesList from './EpisodesList';
 
 import styles from './App.module.scss';
-
-const API_BASE = 'https://api.tvmaze.com'
-const DATE_BASE = '/schedule?date=';
-const COUNTRY_BASE = '&country=';
-const DEFAULT_COUNTRY = 'US';
 
 const today = moment().format('YYYY-MM-DD');
 
@@ -27,7 +23,7 @@ class App extends Component {
 
   componentDidMount() {
     this.setState({ _loading: true });
-    this.sendQuery(API_BASE + DATE_BASE + today + COUNTRY_BASE + DEFAULT_COUNTRY);
+    this.sendQuery(api.API_BASE + api.DATE_BASE + today + api.COUNTRY_BASE + api.DEFAULT_COUNTRY);
   }
 
   sendQuery = url => {
