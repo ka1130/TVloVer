b0b1596a9943c3a52a6633e1eae94f0e73bc3883
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-07 17:23:25 +0200

    Remove commented out code

--- src/constants/apiQueries.js
+++ src/constants/apiQueries.js
@@ -1,16 +1,6 @@
 import { format } from 'date-fns';
 
-// export const API_BASE = 'https://api.tvmaze.com/shows'
 export const API_BASE = 'https://api.tvmaze.com'
-
-// The two below constants may be gone
-// once manual limiting to 12 records is introduced
-// working example with API pagination:
-// fetch(api.API_BASE + api.PAGE_BASE + api.DEFAULT_PAGE + api.COUNTRY_BASE + api.DEFAULT_COUNTRY + api.DATE_BASE + api.today)
-// export const PAGE_BASE = '?page=';
-// export const DEFAULT_PAGE = 1;
-// this number will be converted to string but it's easier to deal with numbers
-// if the user is to back and forth on paginated results
 
 export const COUNTRY_BASE = '/schedule?country=';
 export const DEFAULT_COUNTRY = 'US';
