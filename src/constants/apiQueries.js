1621c1fb7ce0c94d86b7c87b0d332108b6ea6370
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-07 11:49:55 +0200

    Set the maximum width of the main app's container

--- src/constants/apiQueries.js
+++ src/constants/apiQueries.js
@@ -2,7 +2,9 @@ import { format } from 'date-fns';
 
 export const API_BASE = 'https://api.tvmaze.com/shows'
 export const PAGE_BASE = '?page=';
-export const DEFAULT_PAGE = '1';
+export const DEFAULT_PAGE = 1;
+// this number will be converted to string but it's easier to deal with numbers
+// if the user is to back and forth on paginated results
 
 export const COUNTRY_BASE = '/schedule?country=';
 export const DEFAULT_COUNTRY = 'US';
