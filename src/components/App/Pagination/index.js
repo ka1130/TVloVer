ec2ba786b64d60fec1522465fdddb056406055bf
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-08 22:47:25 +0200

    Rewrite for loop with a while loop

--- src/components/App/Pagination/index.js
+++ src/components/App/Pagination/index.js
@@ -1,34 +1,19 @@
 import React, { Component } from 'react';
-// import classNames from 'classnames/bind';
 
 import styles from './Pagination.module.scss';
 
 class Pagination extends Component {
   renderPageNumbers = () => {
     const { episodes, episodesPerPage, handlePageChange, currentPage } = this.props;
-    // let cx = classNames.bind(styles);
-
-    // let pageClassName = cx({
-    //   active: props.isActive,
-    // });
-
-    // var euros = [29.76, 41.85, 46.5];
-    // // const sum = euros.reduce((total, amount) => total + amount); 
-    // var sum = euros.reduce( function(total, amount){
-    //   return total + amount
-    // });
-    // sum // 118.11
 
     let pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(episodes.length / episodesPerPage); i++) {
-      pageNumbers.push(i);
-    }
+    let i = 1;
+    while(i <= Math.ceil(episodes.length / episodesPerPage)) pageNumbers.push(i++);
 
     return pageNumbers.map(
       number => <li key={number}
                     id={number}
                     onClick={handlePageChange}
-                    // isActive={currentPage == number}
                     className={currentPage == number ? `${styles.active}` : ''}
                     >
                       {number}
