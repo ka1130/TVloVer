8a5cedd88e24c86bd0130a65c792c852b1f77e3a
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-22 16:21:45 +0200

    Adjust watchlist's and header's styles for mobile devices

--- src/components/App/Header/UserMenu/index.js
+++ src/components/App/Header/UserMenu/index.js
@@ -7,8 +7,10 @@ const UserMenu = props => {
   return (
     <nav role="navigation" className={styles.userMenuWrapper}>
       <ul className={styles.menuList}>
-        <li className={styles.myAccount}>
-          <a href="#">My account</a>
+        <li>
+          <span className={styles.myAccount}>
+            <span>My account</span>
+          </span>
           <ul className={styles.dropdown}>
             <li><Link to="/watchlist">Watchlist</Link></li>
             <li><a href="#">Sign out</a></li>
