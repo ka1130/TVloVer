a115f519845bfb8b47791a9b6e70b9430600d3ae
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-22 22:05:03 +0200

    Remove unused menu item

--- src/components/App/Header/UserMenu/index.js
+++ src/components/App/Header/UserMenu/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import styles from './UserMenu.module.scss';
 
-const UserMenu = props => {
+const UserMenu = () => {
   return (
     <nav role="navigation" className={styles.userMenuWrapper}>
       <ul className={styles.menuList}>
@@ -13,7 +13,6 @@ const UserMenu = props => {
           </span>
           <ul className={styles.dropdown}>
             <li><Link to="/watchlist">Watchlist</Link></li>
-            <li><a href="#">Sign out</a></li>
           </ul>
         </li>
       </ul>
