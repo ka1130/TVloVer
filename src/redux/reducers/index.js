d667fd3a4fd0915c5764e90e861822cd15fd688e
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-01 22:41:36 +0100

    Implement a login simulation

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import data from 'redux/reducers/episodesReducer';
 import show from 'redux/reducers/showsReducer';
 import watchlist from 'redux/reducers/watchlistReducer';
+import changeAuth from 'redux/reducers/authReducer';
 
 const rootReducer = combineReducers({
   data,
   show,
-  watchlist
+  watchlist,
+  auth: changeAuth
 });
 
 export default rootReducer;
